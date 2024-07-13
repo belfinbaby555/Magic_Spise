7c309954049710cdd4bcc541b1d434bc291ee2bf
Belfin <belfinbaby666@gmail.com>
2024-07-13 23:06:05 +0530

    responsive

--- src/Components/product components/Product.jsx
+++ src/Components/product components/Product.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import square from "../../Assets/Images/icons/square.svg";
 import Prod from './productCardv2'
 import axios from "axios";
+import home from "../../Assets/home.module.css"
 
 const Products = () => {
 
@@ -14,46 +15,46 @@ useEffect(()=>{
   
   
   return (
-    <div className="flex flex-row bg-gray-100 pt-[60px]">
-      <div className="w-96 h-96 p-4 pl-10 sticky">
-        <div className="sticky">
-        <h2 className="text-xl font-bold my-10 flex gap-2">
+    <div className="flex sm:flex-row flex-col bg-gray-100 pt-[60px]">
+      <div className="sm:w-96 sm:h-96 sm:p-4 pl-1 sm:pl-10">
+        <div>
+        <h2 className="text-xl font-bold my-10 sm:flex hidden gap-2">
           <img src={square} alt="" className="w-6 object-contain" />
           <span>Categories</span>
         </h2>
-        <ul className="flex flex-col gap-5 ">
-          <li className="rounded-full w-fit px-5 py-1 bg-blue-200 cursor-pointer">
+        <ul className={"flex sm:flex-col flex-row gap-1 sm:gap-5 overflow-x-scroll "+home.category}>
+          <li className="rounded-full w-fit px-5 py-1 text-white bg-blue-600 cursor-pointer select-none h-fit whitespace-nowrap">
             All Products
           </li>
-          <li className="rounded-full w-fit px-5 py-1 hover:bg-blue-200 cursor-pointer ">
+          <li className="rounded-full w-fit px-5 py-1 hover:bg-blue-600 hover:text-white bg-slate-300 select-none cursor-pointer  h-fit whitespace-nowrap ">
             Whole Spices
           </li>
-          <li className="rounded-full w-fit px-5 py-1 hover:bg-blue-200 cursor-pointer">
+          <li className="rounded-full w-fit px-5 py-1 hover:bg-blue-600 hover:text-white bg-slate-300 select-none cursor-pointer  h-fit whitespace-nowrap">
             Nuts & Seeds
           </li>
-          <li className="rounded-full w-fit px-5 py-1 hover:bg-blue-200 cursor-pointer">
+          <li className="rounded-full w-fit px-5 py-1 hover:bg-blue-600 hover:text-white bg-slate-300 select-none cursor-pointer  h-fit whitespace-nowrap">
             Tea & Coffee
           </li>
-          <li className="rounded-full w-fit px-5 py-1 hover:bg-blue-200 cursor-pointer">
+          <li className="rounded-full w-fit px-5 py-1 hover:bg-blue-600 hover:text-white bg-slate-300 select-none cursor-pointer  h-fit whitespace-nowrap">
             Masalas
           </li>
-          <li className="rounded-full w-fit px-5 py-1 hover:bg-blue-200 cursor-pointer">
+          <li className="rounded-full w-fit px-5 py-1 hover:bg-blue-600 hover:text-white bg-slate-300 select-none cursor-pointer h-fit whitespace-nowrap">
             Health & Wellness
           </li>
-          <li className="rounded-full w-fit px-5 py-1 hover:bg-blue-200 cursor-pointer">
+          <li className="rounded-full w-fit px-5 py-1 hover:bg-blue-600 hover:text-white bg-slate-300 select-none cursor-pointer h-fit whitespace-nowrap">
             Others
           </li>
         </ul>
       </div>
       </div>
-      <div className="w-4/5 p-4">
-        <h1 className="text-2xl font-bold text-gray-900 mt-5 border-l-4 pl-2 border-blue-700">
+      <div className="sm:w-4/5 sm:p-4">
+        <h1 className="text-2xl font-bold text-gray-900 mt-5 border-l-4 pl-2 ml-4 sm:ml-0 border-blue-700">
           Top Picks
         </h1>
-        <p className="text-gray-600 text-sm font-medium mt-3">
+        <p className="text-gray-600 text-sm sm:block hidden font-medium mt-3">
           Explore Our Diverse Range of Premium Spice Offerings.
         </p>
-        <div className="grid grid-cols-3 gap-y-10 py-10">
+        <div className="grid sm:grid-cols-3 sm:gap-y-10 py-5 sm:py-10">
           {products.map((product) => (
             <Prod key={product.name} product={product} />
           ))}
